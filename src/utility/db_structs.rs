3503b7f7447b85dfda713a74fc0042575065efd5
zokhcat <understandingzak@gmail.com>
2024-07-30 17:18:01 +0530

    chore(structs): Structs for Budget and Expense database operations

--- src/utility/db_structs.rs
+++ src/utility/db_structs.rs
@@ -12,3 +12,34 @@ pub struct LoginInfo {
     pub username: String,
     pub password: String,
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct UpdateUser {
+    pub username: Option<String>,
+    pub password: Option<String>,
+    pub email: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct NewBudget {
+    pub name: String,
+    pub total_amount: f64,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct UpdateBudget {
+    pub name: Option<String>,
+    pub total_amount: Option<f64>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct NewExpense {
+    pub amount: f64,
+    pub description: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct UpdateExpense {
+    pub amount: Option<f64>,
+    pub description: Option<String>,
+}
